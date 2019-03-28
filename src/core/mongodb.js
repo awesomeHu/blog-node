568f47893af3ab603ae4c0f7fd7c65a8158ae32d
awesomeHu <hwkgoook@gmail.com>
2019-03-28 16:10:14 -0400

    fix assignment bug

--- src/core/mongodb.js
+++ src/core/mongodb.js
@@ -9,7 +9,7 @@ exports.mongoose = mongoose
 //connect
 exports.connect = () => {
 
-    const dbURI = 'mongodb://127.0.0.1:27017/blog_node';
+    let dbURI = 'mongodb://127.0.0.1:27017/blog_node';
     if (process.env.NODE_ENV === 'production') {
         dbURI = 'mongodb://heroku_r51dtnk0:3gchv8be1id51c2lfpiba122mp@ds121105.mlab.com:21105/heroku_r51dtnk0';
     }
